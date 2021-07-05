0e2f2904f2a41cfe0c6d2a07a4039f42e43ac457
vsm <devops.july2.2017@gmail.com>
2021-07-05 11:01:58 +0530

    Update facebook.java

--- webapp-addresbbokproject/src/test/java/TestNG/facebook.java
+++ webapp-addresbbokproject/src/test/java/TestNG/facebook.java
@@ -1,79 +1,77 @@
 package TestNG;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class facebook {
+public class google {
 	 WebDriver driver;
 	@BeforeMethod
-	public void launch() {
+	public void launch() throws MalformedURLException, InterruptedException {
 		System.setProperty("webdriver.gecko.driver", "/Users/ravi/Downloads/geckodriver");
-		  driver = new FirefoxDriver();
-		  driver.get("http://www.facebook.com");
+		//  driver = new FirefoxDriver();
+		DesiredCapabilities cap = DesiredCapabilities.firefox();
+
+		driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), cap);
+		  driver.get("http://www.google.com");
 		  driver.manage().window().maximize();
 		  driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
+		 
 	}
   @Test
-  public void facebook1() throws InterruptedException {
+  public void google1() throws InterruptedException {
 	  
-	  driver.findElement(By.name("email")).sendKeys("ravi@gmail.com");
-      Thread.sleep(100);
-      driver.findElement(By.name("pass")).sendKeys("Password");
-      Thread.sleep(100);
-
-      driver.findElement(By.id("loginbutton")).click();
+	  if (driver.getPageSource().contains("I'm Feeling Lucky")) {
+          System.out.println("Pass");
+          }
+          else {
+           System.out.println("Fail");
+                }
+     
+      Thread.sleep(600);	 	  	  
+  }
+  
+  @Test
+  public void google2() throws InterruptedException {
+	  
+	  if (driver.getPageSource().contains("I'm Feeling Lucky")) {
+          System.out.println("Pass");
+  } else {
+          System.out.println("Fail");
+}
+     
       Thread.sleep(600);	 	  	  
   }
   @Test
-  public void facebook0() throws InterruptedException {
+  public void google3() throws InterruptedException {
 	  
-	  driver.findElement(By.name("email")).sendKeys("ravi@gmail.com");
-      Thread.sleep(100);
-      driver.findElement(By.name("pass")).sendKeys("Password");
-      Thread.sleep(100);
-
-      driver.findElement(By.id("loginbutton")).click();
+	  if (driver.getPageSource().contains("I'm Feeling Lucky")) {
+          System.out.println("Pass");
+  } else {
+          System.out.println("Fail");
+}
+     
       Thread.sleep(600);	 	  	  
   }
   @Test
-  public void facebook2() throws InterruptedException {
+  public void google4() throws InterruptedException {
 	  
-	  driver.findElement(By.name("email")).sendKeys("ravi@gmail.com");
-      Thread.sleep(100);
-      driver.findElement(By.name("pass")).sendKeys("Password");
-      Thread.sleep(100);
-
-      driver.findElement(By.id("loginbutton")).click();
+	  if (driver.getPageSource().contains("I'm Feeling Lucky")) {
+          System.out.println("Pass");
+  } else {
+          System.out.println("Fail");
+}
+     
       Thread.sleep(600);	 	  	  
   }
-  @Test
-  public void facebook3() throws InterruptedException {
-	  
-	  driver.findElement(By.name("email")).sendKeys("ravi@gmail.com");
-      Thread.sleep(100);
-      driver.findElement(By.name("pass")).sendKeys("Password");
-      Thread.sleep(100);
-
-      driver.findElement(By.id("loginbutton")).click();
-      Thread.sleep(600);	 	  	  
-  }
-  @Test
-  public void facebook4() throws InterruptedException {
-	  
-	  driver.findElement(By.name("email")).sendKeys("ravi@gmail.com");
-      Thread.sleep(100);
-      driver.findElement(By.name("pass")).sendKeys("Password");
-      Thread.sleep(100);
-
-      driver.findElement(By.id("loginbutton")).click();
-      Thread.sleep(600);	 	  	  
-  }
- 
    @AfterMethod
     public void logout() {
 	    driver.quit();
